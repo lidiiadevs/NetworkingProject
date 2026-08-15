ccb96a902e607982d462c9ab0fca2be3f1ac17bb
Lidiia Diachkovskaia <lyatlasova@gmail.com>
2026-08-15 13:00:42 -0400

    Initial Commit

---
+++ NetworkingProject/NetworkingProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkingProjectApp.swift
+//  NetworkingProject
+//
+//  Created by Lidiia Diachkovskaia on 8/15/26.
+//
+
+import SwiftUI
+
+@main
+struct NetworkingProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
